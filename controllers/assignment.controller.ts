86571dbd5be88d6800c81a0ba1b08311f8c1dcd6
Matthew Mahendra <13521007@std.stei.itb.ac.id>
2023-11-04 11:09:04 +0700

    feat: Assignment Controller

--- controllers/assignment.controller.ts
+++ controllers/assignment.controller.ts
@@ -38,21 +38,35 @@ export const getAssignment = async (req: Request, res: Response) => {
     }
 }
 
-// export const createAssignment = async(req: Request, res: Response) => {
-//     try{
-//         const {sid} = req.params;
-//         const {desc, name} = req.body;
-//         const assignment = await prisma.assignment.create(
-//             {
-//                 data:{
+export const createAssignment = async(req: Request, res: Response) => {
+    try{
+        const { organization_id, scholarship_id, name,  description  } = req.body
 
-//                 }
-//             }
-//         );
-//     }catch (error: any){
-//         res.status(400).json({
-//             status: "error",
-//             message: error.message,
-//         });
-//     }
-// }
+        const assignment = await prisma.assignment.create({
+            data: {
+              organization_id,
+              scholarship_id,
+              name,
+              desc: description, 
+            },
+          })
+
+          res.status(200).json({
+            status: "success",
+            message: "Assignment created successfully",
+            data: {
+                assignment_id: assignment.assignment_id,
+                organization_id: assignment.organization_id,
+                assignment_name: assignment.name,
+                assignment_description: assignment.desc,
+                scholarship_id: assignment.scholarship_id
+            },
+        });
+
+    }catch (error: any){
+        res.status(400).json({
+            status: "error",
+            message: error.message,
+        });
+    }
+}
