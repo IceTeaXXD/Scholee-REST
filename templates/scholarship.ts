d96ef8358bae71724f99c433e9c5442c955d0097
Matthew Mahendra <13521007@std.stei.itb.ac.id>
2023-11-10 00:33:50 +0700

    feat: Scholarship Polling

---
+++ templates/scholarship.ts
@@ -0,0 +1,43 @@
+import dotenv from "dotenv"
+dotenv.config()
+
+const headers = {
+    "Content-Type": "text/xml;charset=UTF-8",
+    "API-Key": process.env.SOAP_API_KEY
+}
+
+const url = process.env.SOAP_URL + "/ws/ScholarshipService?wsdl"
+
+const getAllScholarshipEnvelope = `
+    <Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+        <Body>
+            <getAllScholarship xmlns="http://services.soap.orden.com/">
+            </getAllScholarship>
+        </Body>
+    </Envelope>
+    `
+
+const setScholarshipRESTTemplate = `
+    <Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+    <Body>
+        <setRESTscholarshipID xmlns="http://services.soap.orden.com/">
+            <user_id_scholarship_php xmlns="">%d</user_id_scholarship_php>
+            <scholarship_id_php xmlns="">%d</scholarship_id_php>
+            <user_id_scholarship_rest xmlns="">%d</user_id_scholarship_rest>
+            <scholarship_id_rest xmlns="">%d</scholarship_id_rest>
+        </setRESTscholarshipID>
+    </Body>
+    </Envelope>
+`
+
+export const getAllScholarship = {
+    url,
+    headers,
+    body: getAllScholarshipEnvelope
+}
+
+export const setScholarshipREST = {
+    url,
+    headers, 
+    body: setScholarshipRESTTemplate
+}
