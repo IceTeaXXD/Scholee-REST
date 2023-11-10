10d4a40ab9e00203d2562266b8b7f71e7e9c778d
Matthew Mahendra <13521007@std.stei.itb.ac.id>
2023-11-10 22:20:19 +0700

    feat: Add File API

--- server.ts
+++ server.ts
@@ -15,6 +15,7 @@ const ScholarshipRoute = require("./routes/scholarship.routes")
 const UniversityRoute = require("./routes/university.routes")
 const AssignmentRoute = require("./routes/assignment.routes")
 const authRoute = require("./routes/auth.routes")
+const fileRoute = require("./routes/files.routes")
 const soapRoute = require("./routes/soap.routes")
 
 app.use(cors({
@@ -32,13 +33,13 @@ app.use(express.json());
 app.use("/", defaultroute);
 app.use("/api", authRoute);
 app.use("/api", soapRoute);
-app.use(verifyJWT);
+// app.use(verifyJWT);
 // app.use("/api", soapRoute);
 app.use("/api", UniversityRoute)
 app.use("/api", ScholarshipRoute)
 app.use("/api", OrganizationRoute)
 app.use("/api", AssignmentRoute)
-
+app.use("/api", fileRoute)
 app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
     sync()
