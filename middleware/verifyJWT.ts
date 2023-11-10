348d6eb8afcf514fbfac3a9108404ef8f9f73c76
henryanandsr <13521004@std.stei.itb.ac.id>
2023-11-10 14:12:22 +0700

    fix : verify JWT

--- middleware/verifyJWT.ts
+++ middleware/verifyJWT.ts
@@ -7,17 +7,13 @@ interface CustomRequest extends Request {
     organization: string | null
 }
 const verifyJWT = (req: Request, res: Response, next: NextFunction): void => {
-    const authHeader = req.headers.authorization || req.headers.Authorization
-    const authHeaderString = Array.isArray(authHeader)
-        ? authHeader[0]
-        : authHeader
+    const accessToken = req.cookies.accToken;
 
-    if (!authHeaderString?.startsWith("Bearer ")) {
+    if (!accessToken) {
         res.sendStatus(401)
     } else {
-        const token = authHeaderString.split(" ")[1]
-        console.log(token)
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string, (err) => {
+        console.log(accessToken)
+        jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET as string, (err : any) => {
             if (err) res.sendStatus(403)
             next()
         })
