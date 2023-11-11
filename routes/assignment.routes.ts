f1a1ffbbc3f1e88179d8648919f328138ac6e876
henryanandsr <13521004@std.stei.itb.ac.id>
2023-11-11 21:40:49 +0700

    fix : move file upload function

--- routes/assignment.routes.ts
+++ routes/assignment.routes.ts
@@ -2,33 +2,15 @@ import express from "express"
 import {
     createAssignment,
     getAssignment,
-    uploadFile,
     getAssignmentBySid
 } from "../controllers/assignment.controller"
-import multer from 'multer';
 
 const router = express.Router()
 
-const upload = multer()
-
 router.post("/assignment/create", createAssignment)
 router.get("/assignment/:sid/:aid", getAssignment)
-router.get("/assignment/:sid", getAssignmentBySid)
-router.post("/assignment/upload", upload.any(), async(req : any,res : any) => {
-    try {
-        console.log(req.body);
-        console.log(req.files);
-        const { body, files } = req;
+router.get("/assignment/:sid", getAssignment)
 
-        for (let f = 0; f < files.length; f += 1) {
-            await uploadFile(files[f]);
-        }
-
-        res.status(200).send("Form Submitted");
-    } catch (error : any) {
-        res.send(error.message);
-    }
-})
 router.get("/assignment/:sid", getAssignmentBySid)
 
 module.exports = router
