eb0b8eb969f9ed3564060c1d28c46049b74b6700
henryanandsr <13521004@std.stei.itb.ac.id>
2023-11-14 17:48:07 +0700

    fix : upload file into google drive

--- controllers/files.controller.ts
+++ controllers/files.controller.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client"
 import stream from "stream"
 import {google} from 'googleapis'
 import path from "path"
+import { file } from "googleapis/build/src/apis/file"
 const prisma = new PrismaClient()
 
 /* URL: /scholarship/:sid/assignment/:aid/file/:fid/user/:uid */
@@ -83,46 +84,52 @@ export const getAllFiles = async (req: Request, res: Response) => {
 /* TODO: Middleware Uploading File */
 export const uploadFiles = async (req: Request, res: Response) => {
     try {
-        const { sid, aid } = req.params
-        const { uid, file_path } = req.body
-
+        const { sid, aid } = req.params;
+        const { uid } = req.body;
+        const { files } : any = req;
+        
         const scholarship = await prisma.scholarship.findUnique({
             where: { scholarship_id: parseInt(sid) }
-        })
+        });
 
         const assignment = await prisma.assignment.findUnique({
             where: { assignment_id: parseInt(aid) }
-        })
+        });
 
         if (!scholarship || !assignment) {
             return res.status(404).json({
                 status: "error",
                 message: "Scholarship or assignment not found."
-            })
+            });
         }
 
-        const savedFile = await prisma.files.create({
-            data: {
-                user_id_student: Number(uid),
-                file_path: "/path/to/your/uploaded/file",
-                organization_id: scholarship.organization_id,
-                scholarship_id: scholarship.scholarship_id,
-                assignment_id: assignment.assignment_id
-            }
-        })
+        const savedFiles = await Promise.all(
+            files.map(async (file: any) => {
+                const fileUrl = await uploadFile(file);
+                return prisma.files.create({
+                    data: {
+                        user_id_student: Number(uid),
+                        file_path: fileUrl,
+                        organization_id: scholarship.organization_id,
+                        scholarship_id: scholarship.scholarship_id,
+                        assignment_id: assignment.assignment_id
+                    } as any
+                });
+            })
+        );
 
         res.status(200).json({
             status: "success",
-            message: "File uploaded successfully.",
-            file: savedFile
-        })
+            message: "Files uploaded successfully.",
+            files: savedFiles
+        });
     } catch (error: any) {
         res.status(400).json({
             status: "error",
             message: error.message
-        })
+        });
     }
-}
+};
 
 const KEYFILEPATH = path.join(__dirname, '..', 'cred.json')
 const SCOPES = ["https://www.googleapis.com/auth/drive"]
@@ -135,16 +142,24 @@ const auth = new google.auth.GoogleAuth({
 export const uploadFile = async (fileObject : any) => {
     const bufferStream = new stream.PassThrough();
     bufferStream.end(fileObject.buffer);
-    const { data } = await google.drive({ version: "v3", auth }).files.create({
-        media: {
-            mimeType: fileObject.mimeType,
-            body: bufferStream,
-        },
-        requestBody: {
-            name: fileObject.originalname,
-            parents: ["16MsCQmIcu4LibyJYMyNeBRXDrRYCs29u"],
-        },
-        fields: "id,name",
-    });
-    console.log(`Uploaded file ${data.name} ${data.id}`);
+
+    try {
+        const drive = google.drive({ version: 'v3', auth });
+        const { data } = await drive.files.create({
+            media: {
+                mimeType: fileObject.mimeType,
+                body: bufferStream,
+            },
+            requestBody: {
+                name: fileObject.originalname,
+                parents: ['16MsCQmIcu4LibyJYMyNeBRXDrRYCs29u'],
+            },
+            fields: 'id,name,webViewLink', 
+        });
+
+        return data.webViewLink;
+    } catch (error : any) {
+        console.error('Error uploading file to Google Drive:', error.message);
+        throw error;
+    }
 };
