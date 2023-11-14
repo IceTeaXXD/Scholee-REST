a36ff66756f214797c4c6d2cfb6862763b7f8f02
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-11-14 15:45:58 +0700

    feat: delete assignment

--- routes/assignment.routes.ts
+++ routes/assignment.routes.ts
@@ -1,10 +1,12 @@
 import express from "express"
 import {
   createAssignment,
+  deleteAssignment
 } from "../controllers/assignment.controller"
 
 const router = express.Router()
 
 router.post("/assignment", createAssignment)
+router.delete("/assignment/:sid/:aid", deleteAssignment)
 
 module.exports = router
