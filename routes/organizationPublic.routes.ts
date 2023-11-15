b0b540873b6330f5237211220d6f1f0607e20a00
henryanandsr <13521004@std.stei.itb.ac.id>
2023-11-15 11:47:28 +0700

    fix : seperate route of create organization

---
+++ routes/organizationPublic.routes.ts
@@ -0,0 +1,11 @@
+import express from "express"
+import {
+    createOrganization,
+} from "../controllers/organization.controller"
+
+const router = express.Router()
+
+router.post("/organization", createOrganization)
+
+
+module.exports = router
